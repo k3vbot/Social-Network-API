6e2ac575c753c696f6d90821a5ca8df72652127b
Kevin <muehl004@gmail.com>
2022-08-31 17:26:31 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+app.use(require('./routes'));
+
+//TODO: Set up mongodb connection
+
+app.listen(PORT, () => console.log('Server UP'));
+
